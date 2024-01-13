0d455b8abbadf71a0b1fd3526e39d8b53e358902
Shubh@m__444 <109231575+ShubhamKorade@users.noreply.github.com>
2024-01-13 09:50:53 +0530

    Add files via upload

---
+++ smooth-scroll.js
@@ -0,0 +1,6 @@
+document.getElementById("myForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+    // Add your form submission logic here (e.g., sending data to a server)
+    alert("Form submitted!");
+  });
+  
